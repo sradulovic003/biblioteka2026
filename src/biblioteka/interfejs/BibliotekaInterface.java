1a92b9c908f04c3e645c9b98abeafa498ed323fc
sradulovic003 <sradulovic003@gmail.com>
2026-03-16 14:49:15 +0100

    Uveden interfejs BibliotekaInterface

---
+++ src/biblioteka/interfejs/BibliotekaInterface.java
@@ -0,0 +1,14 @@
+package biblioteka.interfejs;
+
+import java.util.List;
+
+import biblioteka.Autor;
+import biblioteka.Knjiga;
+
+public interface BibliotekaInterface {
+
+	public void dodajKnjigu(Knjiga knjiga);
+	public void obrisiKnjigu(Knjiga knjiga);
+	public List<Knjiga> vratiSveKnjige();
+	public List<Knjiga> pronadjiKnjigu(Autor autor, long isbn, String naslov, String izdavac);
+}
