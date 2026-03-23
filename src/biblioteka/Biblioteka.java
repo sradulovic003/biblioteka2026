3fc8f5f511f0239a7407531ca7145d492260199f
sradulovic003 <sradulovic003@gmail.com>
2026-03-23 12:48:28 +0100

    Uvedena klasa Biblioteka koja implementira BibliotekaInterface

---
+++ src/biblioteka/Biblioteka.java
@@ -0,0 +1,39 @@
+package biblioteka;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import biblioteka.interfejs.BibliotekaInterface;
+
+public class Biblioteka implements BibliotekaInterface {
+
+	private List<Knjiga> knjige= new ArrayList<Knjiga>();
+	
+	@Override
+	public void dodajKnjigu(Knjiga knjiga) {
+		knjige.add(knjiga);
+
+	}
+
+	@Override
+	public void obrisiKnjigu(Knjiga knjiga) {
+		knjige.remove(knjiga);
+	}
+
+	@Override
+	public List<Knjiga> vratiSveKnjige() {
+		return knjige;
+	}
+
+	@Override
+	public List<Knjiga> pronadjiKnjigu(Autor autor, long isbn, String naslov, String izdavac) {
+		List<Knjiga> rezultati = new ArrayList<Knjiga>();
+		for(Knjiga k:knjige) {
+			if(k.getNaslov().toUpperCase().contains(naslov.toUpperCase())){
+				rezultati.add(k);
+			}
+		}
+		return rezultati;
+	}
+
+}
